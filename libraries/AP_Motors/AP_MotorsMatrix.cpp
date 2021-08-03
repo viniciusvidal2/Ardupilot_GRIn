0c4cc6c3cda3293432e1d6f17a871ac7dbe1c080
Mathaus Ferreira <mathausfsilva@hotmail.com>
2021-08-02 22:13:42 -0300

    add motor_raw, complementar ao commit anterior

--- libraries/AP_Motors/AP_MotorsMatrix.cpp
+++ libraries/AP_Motors/AP_MotorsMatrix.cpp
@@ -515,7 +515,12 @@ void AP_MotorsMatrix::setup_motors(motor_frame_class frame_class, motor_frame_ty
     add_motor(AP_MOTORS_MOT_2, -135, AP_MOTORS_MATRIX_YAW_FACTOR_CCW, 3);
     add_motor(AP_MOTORS_MOT_3, -45, AP_MOTORS_MATRIX_YAW_FACTOR_CW, 4);
     add_motor(AP_MOTORS_MOT_4, 135, AP_MOTORS_MATRIX_YAW_FACTOR_CW, 2);
-    //                    add_motor(AP_MOTORS_MOT_5,    0,                              0,  5); //(mathaus) Adicionando quinto motor
+    
+    add_motor_raw(AP_MOTORS_MOT_9 , 0, 0, 0, 5);
+    add_motor_raw(AP_MOTORS_MOT_10, 0, 0, 0, 6);
+    add_motor_raw(AP_MOTORS_MOT_11, 0, 0, 0, 7);
+    add_motor_raw(AP_MOTORS_MOT_12, 0, 0, 0, 8);
+    
     success = true;
 
     normalise_rpy_factors();
